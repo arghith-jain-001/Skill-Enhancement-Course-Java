b373701c1ce1e702d0183ea4ba1871f243aa4a39
arghith-jain-001 <arghith2020@gmail.com>
2026-08-24 15:28:15 +0530

    array 1D 2D

---
+++ array1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        System.out.print("The elements are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+    }
+}
