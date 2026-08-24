f691f40df6b3b138ae6d27db9b96caeb51155772
arghith-jain-001 <arghith2020@gmail.com>
2026-08-24 14:43:28 +0530

    countEO

---
+++ arrayA.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class arrayA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] a = new int[size];
+
+        System.out.printf("Enter %d values: ", size);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+
+    }
+}
---
+++ countEO.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class countEO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] a = new int[size];
+
+        System.out.printf("Enter %d values: ", size);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        int count_even = 0;
+        int count_odd = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (a[i] % 2 == 0)
+                count_even++;
+            else
+                count_odd++;
+        }
+
+        System.out.printf("Count >> Even: %d | Odd: %d\n", count_even, count_odd);
+
+    }
+}
